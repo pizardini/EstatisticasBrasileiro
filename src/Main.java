5e38bfc4a0ba860c57908eb86613ff3f3b3821a9
rodi38 <rodrigorochaed1@gmail.com>
2023-02-08 21:26:43 -0300

    jogador com maior quantidade de gols feitos

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,10 @@ public class Main {
         CartaoVermelho cartaoVermelho = new CartaoVermelho();
         GolsPenalti golsPenalti = new GolsPenalti();
         GolsContra golsContra = new GolsContra();
-        TimeVencedor timeeVencedor = new TimeVencedor();
+        TimeVencedor timeVencedor = new TimeVencedor();
+
+        MaisGols maisGols = new MaisGols();
+
     }
 
 }
